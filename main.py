648309cfc004ab77d0c542afc12ee41297d97c4b
GabrielG01 <gabrielprovo01@gmail.com>
2019-09-25 10:09:22 -0600

    Imported random, added the random functionality to give random number.

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
+import random
+
 def blackjack():
-    card_list = [1,2,3,4,5,6,7,8,9,10]
+    card_list = [1,2,3,4,5,6,7,8,9,10,11]
     total_balance = 200
     print(f'\nWelcome to the blackjack table!\nYour current balance is ${total_balance}, would you like to play? (Y / N) ')
     userInput = input('').upper()
@@ -8,8 +10,13 @@ def blackjack():
         print('Thanks for stopping by! Come Again!')
         return
 
+    def mainGame(deck, cash):
+        print("Here are your cards sir and/or ma'am!\n")
+        print(f'You are handed a {random.choice(card_list)} and a {random.choice(card_list)}.')
+
     if userInput == 'Y':
-        print('thats cringe')
+        mainGame(card_list,total_balance)
+        
     else:
         endCard()
         

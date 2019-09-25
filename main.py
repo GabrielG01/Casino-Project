98e2a5f4740a4068c30d00ce2966da7693f9a21d
GabrielG01 <gabrielprovo01@gmail.com>
2019-09-25 09:57:17 -0600

    Added the card list, total balance, and the begininng of the actual game

--- main.py
+++ main.py
@@ -1,2 +1,17 @@
 def blackjack():
+    card_list = [1,2,3,4,5,6,7,8,9,10]
+    total_balance = 200
+    print(f'\nWelcome to the blackjack table!\nYour current balance is ${total_balance}, would you like to play? (Y / N) ')
+    userInput = input('').upper()
     
+    def endCard():
+        print('Thanks for stopping by! Come Again!')
+        return
+
+    if userInput == 'Y':
+        print('thats cringe')
+    else:
+        endCard()
+        
+
+blackjack()

1ec0b04729f5794fabff14ec35320bfd6073cd59
GabrielG01 <gabrielprovo01@gmail.com>
2019-09-25 10:29:00 -0600

    Added coments to make it clear what the end goals should be, blackjack's rules

--- main.py
+++ main.py
@@ -1,3 +1,14 @@
+# beat the dealer by getting a count as close to 21 as possible, without going over 21.
+
+# before the deal begins, each player places a bet
+
+# player receives two cards, and the dealer receives one card 
+
+# player must decide whether to "stand" (not ask for another card) or "hit" (ask for another card in an attempt to get closer to a count of 21, or even hit 21 exactly)
+
+# dealer deals another card. If the total is 17 or more, it must stand. If the total is 16 or under, then add cards until its 17 or more
+
+
 import random
 
 def blackjack():
